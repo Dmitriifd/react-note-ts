36e35ebd33565be855a013d8756554f92f8709cd
Dmitriifd <idimka9@gmail.com>
2023-05-06 19:39:25 +0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Hello</h1>} />
+      <Route path="/new" element={<h1>New</h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
